14ccc45047420cddb1322d387596231aede9d51f
Hamed Bakhti <75586135+Hamed-68@users.noreply.github.com>
2022-04-27 11:04:14 +0430

    Update settings.py

--- store/settings.py
+++ store/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates')],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,11 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    (BASE_DIR, 'static'),
-)
-
-STATIC_ROOT = (BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
